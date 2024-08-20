b7db9ce2348fbb2d0108cc2d77aa88a134ba14ee
NILAY DAV NATH <143266738+nilaydavnath69196@users.noreply.github.com>
2024-08-20 22:05:10 +0600

    Add files via upload

---
+++ C++/hello.cpp
@@ -0,0 +1,12 @@
+/* Nlay Dav Nath
+16/08/2024
+*/
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    cout<<"NIlay"<<endl;
+    cout<<"Dav Nath";
+  getch ();
+}
---
+++ C++/user-input.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    int num1, num2;
+cout<< "Enter two number: ";
+cin>> num1 >>num2;
+cout<< "The number are: "<<num1 <<num2;
+getch();
+}
