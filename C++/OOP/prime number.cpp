5c4f21e9ab410a284bfe4892ea31176198627c14
NILAY DAV NATH <143266738+nilaydavnath69196@users.noreply.github.com>
2024-08-30 00:43:57 +0600

    Add files via upload

---
+++ C++/OOP/prime number.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class number
+{
+    public:
+    void result();
+};
+void number::result()
+{
+    int n,i,count;
+    cout <<"Enter a number: ";
+    cin>>n;
+
+    for(i=2;i<n;i++)
+{if(n%i==0)
+{
+    count++;
+    break;
+}
+}
+if(count==0){
+    cout <<n<<" is a prime number";
+}
+else {
+        cout <<n<<" is not a prime number";
+}
+
+}
+main()
+{
+    number obj;
+    obj.result();
+    return 0;
+}
