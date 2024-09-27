69a20f566ce6cc46b657e276e4e552b670a1830a
NILAY DAV NATH <143266738+nilaydavnath69196@users.noreply.github.com>
2024-09-27 12:08:02 +0600

    Add files via upload

---
+++ C++/OOP/setw & endl.cpp
@@ -0,0 +1,27 @@
+/*
+NILAY DAV NATH
+DEPERTMENT OF COMPUTER SCIENCE AND ENGINEERING
+TOPIC - SETW() AND ENDL
+DATE - 27/09/2024
+*/
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+class word {
+public:
+    void display();
+};
+
+void word::display() {
+    cout << setw(20) << "Hello" << endl;// it print 20 space before Hello
+    cout << setw(10) << "World!" << endl; //it print 10 space before Hello
+}
+
+int main() {
+    word m;
+    m.display();
+    return 0;
+}
+
