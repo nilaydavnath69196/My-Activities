0c29ae4260658387253d9e731a111835e0fe9a2c
NILAY DAV NATH <143266738+nilaydavnath69196@users.noreply.github.com>
2024-11-01 23:38:24 +0600

    Add files via upload

---
+++ Data Structure and Algorithm/bobble sort.c
@@ -0,0 +1,42 @@
+/*
+NILAY DAV NATH
+DEPERTMNET OF COMPUTER SCIENCE AND ENGINEERING
+TOPIC -  BUBBLE SORT ALGRITHM IN C
+DATE: 01/11/2024
+*/
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input elements
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Bubble Sort   This is the main condition
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    // Output sorted array
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
