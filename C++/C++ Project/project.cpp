7e145cfcbb9adeaac57246e0a2b334b687dad4c0
NILAY DAV NATH <143266738+nilaydavnath69196@users.noreply.github.com>
2024-11-09 00:44:23 +0600

    Add files via upload

---
+++ C++/C++ Project/project.cpp
@@ -0,0 +1,78 @@
+/*
+NILAY DAV NATH
+TOPIC - PROJECT FOR PRODUCT INFORMATION IN C++ (CALLING TEXT FILE)
+DATE - 09/11/2024
+DEPERTMENT OF COMPUTER SCIENCE AND ENGINEERING
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class Product {
+public:
+    void displayProductInfo(int productCode);
+};
+void Product::displayProductInfo(int productCode)
+
+
+{
+
+    // Display product code list
+    cout << "Here is the product code list: " << endl;
+    cout << " 100 for CPU" << endl;
+    cout << " 101 for Motherboard" << endl;
+    cout << " 102 for Memory" << endl;
+    cout << " 103 for SSD" << endl;
+    cout << " 104 for RAM" << endl;
+    cout << " 105 for HDD" << endl;
+    cout << " 107 for Monitor" << endl;
+    cout << " 108 for Graphics Card" << endl;
+    cout << " 109 for Mouse" << endl;
+    cout << " 110 for Keyboard" << endl;
+    cout << " 111 for Printer" << endl;
+    cout << " 112 for Projector" << endl;
+    cout << " 113 for WiFi Router" << endl;
+
+    // User input for product code
+    cout << "Now Enter product code to display information: ";
+    cin >> productCode;
+
+
+
+
+        ifstream file("products.txt");
+        string line;
+        bool productFound = false;
+
+        if (file.is_open()) {
+            while (getline(file, line)) {
+                if (line == "Code: " + to_string(productCode)) {
+                    productFound = true;
+                    cout << line << endl;
+                    while (getline(file, line) && !line.empty()) {
+                        cout << line << endl;
+                    }
+                    break;
+                }
+            }
+            file.close();
+
+            if (!productFound) {
+                cout << "Product with code " << productCode << " not found." << endl;
+            }
+        } else {
+            cerr << "Unable to open file!" << endl;
+        }
+    }
+
+
+int main() {
+          int code;
+
+    Product obj;
+    // Call the function to display product info
+    obj.displayProductInfo(code);
+
+    return 0;
+}
