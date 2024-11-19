54c1cad207ddb153e73789d2328fae19674f392f
NILAY DAV NATH <143266738+nilaydavnath69196@users.noreply.github.com>
2024-11-19 21:40:57 +0600

    Update project.cpp

--- C++/C++ Project/project.cpp
+++ C++/C++ Project/project.cpp
@@ -4,6 +4,7 @@ TOPIC - PROJECT FOR PRODUCT INFORMATION IN C++ (CALLING TEXT FILE)
 DATE - 09/11/2024
 DEPERTMENT OF COMPUTER SCIENCE AND ENGINEERING
 */
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -11,35 +12,32 @@ using namespace std;
 
 class Product {
 public:
-    void displayProductInfo(int productCode);
+    void displayProductInfo();
 };
-void Product::displayProductInfo(int productCode)
 
+void Product::displayProductInfo() {
+    int productCode;
+    char anotherProduct = 'y';
+cout << "-------Hello Sir! Wllcome to my project-----------" << endl;
+    while (anotherProduct == 'y') {
+        // Display product code list
+        cout << "Here is the product code list: " << endl;
+        cout << " 100 for CPU" << endl;
+        cout << " 101 for Motherboard" << endl;
+        cout << " 102 for Memory" << endl;
+        cout << " 103 for SSD" << endl;
+        cout << " 104 for RAM" << endl;
+        cout << " 105 for HDD" << endl;
+        cout << " 107 for Monitor" << endl;
+        cout << " 108 for Graphics Card" << endl;
+        cout << " 109 for Mouse" << endl;
+        cout << " 110 for Keyboard" << endl;
+        cout << " 111 for Printer" << endl;
+        cout << " 112 for Projector" << endl;
+        cout << " 113 for WiFi Router" << endl;
 
-{
-
-    // Display product code list
-    cout << "Here is the product code list: " << endl;
-    cout << " 100 for CPU" << endl;
-    cout << " 101 for Motherboard" << endl;
-    cout << " 102 for Memory" << endl;
-    cout << " 103 for SSD" << endl;
-    cout << " 104 for RAM" << endl;
-    cout << " 105 for HDD" << endl;
-    cout << " 107 for Monitor" << endl;
-    cout << " 108 for Graphics Card" << endl;
-    cout << " 109 for Mouse" << endl;
-    cout << " 110 for Keyboard" << endl;
-    cout << " 111 for Printer" << endl;
-    cout << " 112 for Projector" << endl;
-    cout << " 113 for WiFi Router" << endl;
-
-    // User input for product code
-    cout << "Now Enter product code to display information: ";
-    cin >> productCode;
-
-
-
+        cout << "Please Enter product code to display information: ";
+        cin >> productCode;
 
         ifstream file("products.txt");
         string line;
@@ -64,15 +62,18 @@ void Product::displayProductInfo(int productCode)
         } else {
             cerr << "Unable to open file!" << endl;
         }
+
+        cout << "Do you want to see other products' information? ";
+        cout << "Press 'y' for yes and 'n' for no: ";
+        cin >> anotherProduct;
     }
 
+    cout << "---------THANK YOU-----------" << endl;
+}
 
 int main() {
-          int code;
-
     Product obj;
-    // Call the function to display product info
-    obj.displayProductInfo(code);
+    obj.displayProductInfo();
 
     return 0;
 }
