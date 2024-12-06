f4431e0eb777a8446543c91afd9bff264a9e6623
NILAY DAV NATH <143266738+nilaydavnath69196@users.noreply.github.com>
2024-12-06 20:41:34 +0600

    Update project.cpp
    This is update for user input

--- C++/C++ Project/project.cpp
+++ C++/C++ Project/project.cpp
@@ -2,7 +2,7 @@
 NILAY DAV NATH
 TOPIC - PROJECT FOR PRODUCT INFORMATION IN C++ (USING TEXT FILE)
 DATE - 09/11/2024
-DEPERTMENT OF COMPUTER SCIENCE AND ENGINEERING
+DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING
 */
 #include <iostream>
 #include <fstream>
@@ -12,6 +12,7 @@ using namespace std;
 class Product {
 public:
     void displayProductInfo();
+    void addProductInfo();
 };
 
 void Product::displayProductInfo() {
@@ -70,10 +71,65 @@ void Product::displayProductInfo() {
     cout << "THANK YOU!" << endl;
 }
 
+void Product::addProductInfo() {
+    ofstream file("products.txt", ios::app);
+    if (!file.is_open()) {
+        cerr << "Unable to open file!" << endl;
+        return;
+    }
+
+    char anotherProduct = 'y';
+    while (anotherProduct == 'y') {
+        int productCode;
+        string productName, productDetails;
+
+        cout << "Enter product code: ";
+        cin >> productCode;
+        cin.ignore(); // To clear the newline character from the input buffer
+        cout << "Enter product name: ";
+        getline(cin, productName);
+        cout << "Enter product details: ";
+        getline(cin, productDetails);
+
+        file << "Code: " << productCode << endl;
+        file << "Name: " << productName << endl;
+        file << "Details: " << productDetails << endl;
+        file << endl;
+
+        cout << "Do you want to add another product? Press 'y' for yes and 'n' for no: ";
+        cin >> anotherProduct;
+    }
+
+    file.close();
+    cout << "Product information added successfully!" << endl;
+}
+
 int main() {
     Product obj;
-    obj.displayProductInfo();
+    int choice;
+
+    do {
+        cout << "\n=== PRODUCT INFORMATION SYSTEM ===" << endl;
+        cout << "1. Display Product Information" << endl;
+        cout << "2. Add New Product Information" << endl;
+        cout << "3. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                obj.displayProductInfo();
+                break;
+            case 2:
+                obj.addProductInfo();
+                break;
+            case 3:
+                cout << "Exiting... Thank you!" << endl;
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+    } while (choice != 3);
 
     return 0;
 }
-
