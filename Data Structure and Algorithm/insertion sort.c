f6535ba1e17868e88d1ac75baa14512a74a902ae
NILAY DAV NATH <143266738+nilaydavnath69196@users.noreply.github.com>
2024-12-10 10:14:21 +0600

    Update insertion sort.c

--- Data Structure and Algorithm/insertion sort.c
+++ Data Structure and Algorithm/insertion sort.c
@@ -1,39 +1,10 @@
 /*
 NILAY DAV NATH
-DEPERTMNET OF COMPUTER SCIENCE AND ENGINEERING
-TOPIC - INSERTION SORT ALGRITHM IN C
+DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING
+TOPIC - INSERTION SORT ALGORITHM IN C
 DATE: 01/11/2024
 */
 #include <stdio.h>
-
-// Function for Insertion Sort
-void insertionSort(int arr[], int size) {
-    int i, key, j;
-
-    // Traverse the array from the second element to the last
-    for (i = 1; i < size; i++) {
-        key = arr[i];  // Store the current element to be inserted into the sorted part
-        j = i - 1;
-
-        // Move elements of arr[0..i-1] that are greater than key
-        // to one position ahead of their current position
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-
-        // Place key in the correct position in the sorted part
-        arr[j + 1] = key;
-    }
-}
-
-// Function to print the array
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
 
 int main() {
     int size;
@@ -51,13 +22,33 @@ int main() {
     }
 
     printf("Original array: \n");
-    printArray(arr, size);
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 
-    // Sorting the array using insertion sort
-    insertionSort(arr, size);
+    // Insertion sort logic directly in main()
+    int i, key, j;
+    for (i = 1; i < size; i++) {
+        key = arr[i];  // Store the current element to be inserted into the sorted part
+        j = i - 1;
+
+        // Move elements of arr[0..i-1] that are greater than key
+        // to one position ahead of their current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+
+        // Place key in the correct position in the sorted part
+        arr[j + 1] = key;
+    }
 
     printf("Sorted array: \n");
-    printArray(arr, size);
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 
     return 0;
 }
