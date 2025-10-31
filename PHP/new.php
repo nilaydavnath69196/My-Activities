cc54b5fcff6a7d6264cb714006d45b7667894d2c
NILAY DAV NATH <143266738+nilaydavnath69196@users.noreply.github.com>
2025-10-31 12:00:41 +0600

    Add files via upload

---
+++ PHP/new.php
@@ -0,0 +1,15 @@
+<?php
+
+$num1= 5;
+$num2 = 10;
+
+if($num1 == 5 && $num2 == 10){
+    echo "The Number is Trure which are ".$num1; echo " and " .$num2;
+}
+else{
+
+ echo "The Number is False";
+
+}
+
+?>
---
+++ PHP/prime number.php
@@ -0,0 +1,16 @@
+<?php
+
+for($i=0;$i<=100;$i++){
+if($i==1){
+    echo "1"; echo " ";
+}
+else if($i%2==0){
+    echo "";
+}
+else{
+    echo $i; echo" ";
+}
+
+}
+
+?>
